85acdd367860afd268221eb469288e4643e549b9
sunshine <1171626250@qq.com>
2021-03-13 17:37:44 +0800

    update

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="wrap">
-    <label class="l-title">欢迎登录！</label>
-    <div class="l-content">
-      <form action=""></form>
+    <div class="">
+      <label class="l-title">欢迎登录！</label>
+      <div class="l-content">
+        <div style="margin-bottom:20px">
+          <input
+            type="text"
+            class="f-input"
+            placeholder="请输入账号"
+            v-model="form.username"
+          />
+        </div>
+        <div style="margin-bottom:20px">
+          <input
+            type="text"
+            class="f-input"
+            placeholder="请输入密码"
+            v-model="form.username"
+          />
+        </div>
+        <div style="margin-bottom:20px">
+          <button type="button" class="btn btn-success">登录</button>
+        </div>
+        <div>
+          <span>忘记密码？</span>
+          <span>没有账号，去<span>注册</span></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,7 +35,12 @@
 export default {
   name: "login",
   data() {
-    return {};
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+    };
   },
   methods: {},
 };
@@ -29,10 +58,15 @@ export default {
   .l-content {
     width: 410px;
     padding: 40px 50px;
-    background-color: rgb(255, 255, 255);
-    opacity: 0.3;
+    background: rgba(255, 255, 255, 0.5);
     border-radius: 14px;
-    // position: relative;
   }
+}
+.f-input {
+  background: none;
+  height: 40px;
+  border: 1px solid #fff;
+  width: 100%;
+  border-radius: 24px;
 }
 </style>
