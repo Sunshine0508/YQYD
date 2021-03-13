39d670dff75c240f531f1c30c2a690992cb17e24
sunshine <1171626250@qq.com>
2021-03-13 15:55:16 +0800

    update

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrap"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.wrap {
+  background: #8ea7e6;
+  height: 100vh;
+  width: 100%;
+}
+</style>
